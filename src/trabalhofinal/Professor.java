bff1af1559de943d042f1b542358b4da69640b5f
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2020-08-22 11:49:25 -0300

    Adicionados os métodos override de Pessoa em Professor

--- src/trabalhofinal/Professor.java
+++ src/trabalhofinal/Professor.java
@@ -40,4 +40,51 @@ public class Professor extends Pessoa{
     public void setStatus(String status) {
         this.status = status;
     }
+
+    @Override
+    public void bemVindo() {
+        super.bemVindo(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setTelefone(long telefone) {
+        super.setTelefone(telefone); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long getTelefone() {
+        return super.getTelefone(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getEmail() {
+        return super.getEmail(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setNome(String nome) {
+        super.setNome(nome); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNome() {
+        return super.getNome(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCpf(int cpf) {
+        super.setCpf(cpf); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getCpf() {
+        return super.getCpf(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
 }
